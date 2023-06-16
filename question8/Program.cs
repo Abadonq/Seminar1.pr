277283530d2a7b9badbbd370d0b22060bbdbe57c
Кузилин Юра <yurakuzilin@gmaio.com>
2023-06-16 03:46:27 +0300

    solve 8th question

---
+++ question8/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 8
+
+int N = new Random().Next(1,100); 
+int count=1;
+
+while (count < N+1)
+{
+    if (count%2==0)
+    {
+        Console.Write($"{count}, ");
+    }
+    count++;
+}
+
+Console.WriteLine($"Загадываемое число:{N}");
+
+
