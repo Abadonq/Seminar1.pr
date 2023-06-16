7f3eea7064f1d4a13237ab12540f6bf17a368f63
Кузилин Юра <yurakuzilin@gmaio.com>
2023-06-16 03:08:31 +0300

    solve 1st question

---
+++ Question1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 2
+
+int num1 = new Random().Next(1,10); 
+int num2 = new Random().Next(1,10); 
+
+if (num1 > num2) 
+{
+     Console.WriteLine($"{num1} больше {num2} ");
+}
+else
+{
+     Console.WriteLine($"{num2} больше {num1} ");
+}
+
+
+
